7b3d8369aa7dc08a8a18a89439adf8952601e17a
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2024-02-17 21:27:31 +0000

    refactor: how to add custom fonts

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -23,11 +23,22 @@ class MyApp extends StatelessWidget {
               Text(
                 'Marquinhus',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 40.0,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
-              )
+              ),
+              Text(
+                'SOFTWARE ENGINEER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans 3',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ],
           ),
         ),
