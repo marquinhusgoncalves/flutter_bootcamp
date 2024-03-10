4e9123e110c2b2a784d970242bf6c5dc2574f7f8
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2024-03-10 18:31:54 +0000

    feat: composition vs inheritance

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -14,6 +14,8 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   Gender? selectedGender;
   int height = 180;
+  int weight = 60;
+  int age = 20;
 
   @override
   Widget build(BuildContext context) {
@@ -105,14 +107,48 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     color: kActiveCardColor,
-                    cardChild: IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('WEIGHT', style: kLabelTextStyle),
+                        Text(
+                          weight.toString(),
+                          style: kNumberTextStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            RoundIconButton(icon: FontAwesomeIcons.minus, onPressed: () => setState(() => weight--)),
+                            SizedBox(width: 10.0),
+                            RoundIconButton(icon: FontAwesomeIcons.plus, onPressed: () => setState(() => weight++)),
+                          ],
+                        ),
+                      ],
+                    ),
                     onPress: () => {},
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     color: kActiveCardColor,
-                    cardChild: IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('AGE', style: kLabelTextStyle),
+                        Text(
+                          age.toString(),
+                          style: kNumberTextStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            RoundIconButton(icon: FontAwesomeIcons.minus, onPressed: () => setState(() => age--)),
+                            SizedBox(width: 10.0),
+                            RoundIconButton(icon: FontAwesomeIcons.plus, onPressed: () => setState(() => age++)),
+                          ],
+                        ),
+                      ],
+                    ),
                     onPress: () => {},
                   ),
                 ),
@@ -127,6 +163,25 @@ class _InputPageState extends State<InputPage> {
         width: double.infinity,
         height: kBottomContainerHeight,
       ),
+    );
+  }
+}
+
+class RoundIconButton extends StatelessWidget {
+  const RoundIconButton({required this.icon, required this.onPressed});
+
+  final IconData icon;
+  final void Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      elevation: 0.0,
+      child: Icon(icon),
+      onPressed: onPressed,
+      constraints: BoxConstraints.tightFor(width: 56.0, height: 56.0),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
     );
   }
 }
