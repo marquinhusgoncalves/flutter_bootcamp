309178229f69c9625332d83c25a00359df232339
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2024-03-10 19:39:44 +0000

    feat: designing the result page

--- bmi_calculator/lib/screens/input_page.dart
+++ bmi_calculator/lib/screens/input_page.dart
@@ -1,6 +1,8 @@
+import 'package:bmi_calculator/components/bottom_button.dart';
+import 'package:bmi_calculator/components/icon_content.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
+import 'package:bmi_calculator/components/round_icon_button.dart';
 import 'package:bmi_calculator/constants.dart';
-import 'package:bmi_calculator/icon_content.dart';
-import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -157,37 +159,12 @@ class _InputPageState extends State<InputPage> {
           ),
         ],
       ),
-      bottomNavigationBar: GestureDetector(
+      bottomNavigationBar: BottomButton(
+        buttonTitle: 'CALCULATE',
         onTap: () {
           Navigator.pushNamed(context, '/results');
         },
-        child: Container(
-          child: Text('CALCULATE'),
-          color: kBottomContainerColor,
-          margin: EdgeInsets.only(top: 10.0),
-          width: double.infinity,
-          height: kBottomContainerHeight,
-        ),
       ),
-    );
-  }
-}
-
-class RoundIconButton extends StatelessWidget {
-  const RoundIconButton({required this.icon, required this.onPressed});
-
-  final IconData icon;
-  final void Function() onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      elevation: 0.0,
-      child: Icon(icon),
-      onPressed: onPressed,
-      constraints: BoxConstraints.tightFor(width: 56.0, height: 56.0),
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
     );
   }
 }
