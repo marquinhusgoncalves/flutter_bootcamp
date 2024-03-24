6230252f34a7f6b87365c7d216445a9dbcbf74aa
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2024-03-24 17:26:14 +0000

    refactor: async and await challenge

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,23 +7,22 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Location location = Location();
+
   @override
   void initState() {
     super.initState();
     getLocation();
   }
 
+  void getLocation() async {
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
-  }
-
-  void getLocation() async {
-    try {
-      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-      print(position);
-    } catch (e) {
-      print(e);
-    }
   }
 }
