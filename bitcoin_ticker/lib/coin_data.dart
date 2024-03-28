f375d1287d61b02b9cbd9d852f7489a9a820a570
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2024-03-28 23:20:16 +0000

    challenge level 1 - bronze medal

--- bitcoin_ticker/lib/coin_data.dart
+++ bitcoin_ticker/lib/coin_data.dart
@@ -1,3 +1,11 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+// Get your API key from https://www.coinapi.io/
+const apiKey = 'YOUR-API-KEY';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +36,19 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Future getData() async {
+    String url = '$coinAPIURL/BTC/USD?apikey=$apiKey';
+    final Uri requestURL = Uri.parse(url);
+    http.Response response = await http.get(requestURL);
+
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData['rate'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+      throw 'Problem with the get request';
+    }
+  }
+}
--- bitcoin_ticker/lib/price_screen.dart
+++ bitcoin_ticker/lib/price_screen.dart
@@ -11,6 +11,13 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
+  String bitcoinValueInUSD = '?';
+
+  @override
+  void initState() {
+    super.initState();
+    getCoinData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +40,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $bitcoinValueInUSD USD ',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -53,6 +60,17 @@ class _PriceScreenState extends State<PriceScreen> {
         ],
       ),
     );
+  }
+
+  void getCoinData() async {
+    try {
+      double data = await CoinData().getData();
+      setState(() {
+        bitcoinValueInUSD = data.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 
   DropdownButton<String> androidDropdown() {
