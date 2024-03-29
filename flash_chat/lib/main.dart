33a0547a5ffc4e9d34abbd32c9e193619807a13d
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2024-03-29 19:57:29 +0000

    refactor: use named route

--- flash_chat/lib/main.dart
+++ flash_chat/lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 // import 'package:flash_chat/screens/login_screen.dart';
@@ -15,7 +18,13 @@ class FlashChat extends StatelessWidget {
           bodySmall: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: 'welcome_screen',
+      routes: {
+        'welcome_screen': (context) => WelcomeScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'registration_screen': (context) => RegistrationScreen(),
+        'chat_screen': (context) => ChatScreen(),
+      },
     );
   }
 }
